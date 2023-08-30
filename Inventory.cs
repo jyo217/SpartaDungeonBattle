937966ce89982ba270f44ac363fe3dedd2e5b0c6
jyo217 <solbaram217@gmail.com>
2023-08-30 16:04:02 +0900

    Inventory.cs - ManageEquipment, StateManager.cs - ManageEquipment 수정#4

--- Inventory.cs
+++ Inventory.cs
@@ -17,8 +17,8 @@ namespace SpartaDungeonBattle
         public Inventory()
         {
             ItemList = new List<Item>();
-            ItemList.Add(new Equipment(ItemType.Armor, "무쇠갑옷", 100, "무쇠로 만들어져 튼튼한 갑옷입니다.", 0, 5));
-            ItemList.Add(new Equipment(ItemType.Weapon, "낡은 검", 100, "쉽게 볼 수 있는 낡은 검 입니다.", 2, 0));
+            ItemList.Add(new Equipment("무쇠갑옷", 100, "무쇠로 만들어져 튼튼한 갑옷입니다.", 0, 5));
+            ItemList.Add(new Equipment("낡은 검", 100, "쉽게 볼 수 있는 낡은 검 입니다.", 2, 0));
             ItemList.Add(Consumption.MakePotion());
         }
         public Inventory(List<Item> itemList) { ItemList = itemList; }
@@ -30,5 +30,32 @@ namespace SpartaDungeonBattle
         }
 
         public int Count { get { return ItemList.Count; } }
+
+        /// <summary>
+        /// 장비 장착, 탈착 기능
+        /// </summary>
+        public void ManageEquipment(int index)
+        {
+            //이걸 장비 타입으로 캐스팅 시도
+            Equipment equipment = ItemList[index] as Equipment;
+
+            //캐스팅 성공 시 장비 장착/탈착 수행
+            if (equipment != null)
+            {
+                //장착 중인 아이템이라면 장착 상태 변경 및 장착 아이템 리스트에서 제거
+                if(equipment.OnEquipped)
+                {
+                    Character.CurrentCharacter.ItemOnEquipped.Remove(equipment);
+                    equipment.OnEquipped = false;
+                }
+                //미장착 아이템이라면 장착 상태 변경 및 장착 아이템 리스트에 추가
+                else
+                {
+                    Character.CurrentCharacter.ItemOnEquipped.Add(equipment);
+                    equipment.OnEquipped = true;
+                }
+            }
+            else { Console.WriteLine("Item => Equipment 타입 캐스팅 실패!!!"); }
+        }
     }
 }
